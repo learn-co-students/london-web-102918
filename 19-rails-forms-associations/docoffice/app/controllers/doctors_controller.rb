0b287ac54b5697a4610f8a2bc8edf52fdaa3ece3
danielkaczmarczyk <danielkaczmarczyk2@gmail.com>
2018-11-27 15:44:30 +0000

    edits

---
+++ 19-rails-forms-associations/docoffice/app/controllers/doctors_controller.rb
@@ -0,0 +1,42 @@
+class DoctorsController < ApplicationController
+  before_action :find_doctor, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @doctors = Doctor.all
+  end
+
+  def show
+  end
+
+  def new
+    @doctor = Doctor.new
+  end
+
+  def create
+    @doctor = Doctor.create(doctor_params)
+    redirect_to doctor_path(@doctor)
+  end
+
+  def edit
+  end
+
+  def update
+    @doctor.update(doctor_params)
+    redirect_to doctor
+  end
+
+  def destroy
+    @doctor.destroy
+    redirect_to doctors_path
+  end
+
+  private
+
+  def doctor_params
+    params.require(:doctor).permit(:name, :specialty)
+  end
+
+  def find_doctor
+    @doctor = Doctor.find(params[:id])
+  end
+end
